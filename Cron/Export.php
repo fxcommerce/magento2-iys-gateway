92a6255df2e123dd7db5f0aa618b9c03d9ba7232
Serkan Akyol <serkanakyol@gmail.com>
2026-07-16 12:44:26 +0300

    Run bounded bidirectional synchronization in cron

--- Cron/Export.php
+++ Cron/Export.php
@@ -5,14 +5,18 @@ namespace FxCommerce\IysGateway\Cron;
 
 use FxCommerce\IysGateway\Model\Config;
 use FxCommerce\IysGateway\Model\Gateway\Exporter;
+use FxCommerce\IysGateway\Model\Gateway\InboundSynchronizer;
 use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface;
 
 class Export
 {
     public function __construct(
         private readonly Config $config,
         private readonly Exporter $exporter,
-        private readonly StoreManagerInterface $storeManager
+        private readonly InboundSynchronizer $inboundSynchronizer,
+        private readonly StoreManagerInterface $storeManager,
+        private readonly LoggerInterface $logger
     ) {
     }
 
@@ -20,8 +24,26 @@ class Export
     {
         foreach ($this->storeManager->getStores() as $store) {
             $storeId = (int)$store->getId();
-            if ($this->config->isCronEnabled($storeId)) {
+            if (!$this->config->isCronEnabled($storeId) || !$this->config->isEnabled($storeId)) {
+                continue;
+            }
+
+            try {
+                $this->inboundSynchronizer->execute($storeId, 100);
+            } catch (\Throwable $exception) {
+                $this->logger->error('IYS Gateway inbound cron synchronization failed.', [
+                    'store_id' => $storeId,
+                    'exception' => $exception,
+                ]);
+            }
+
+            try {
                 $this->exporter->execute($storeId);
+            } catch (\Throwable $exception) {
+                $this->logger->error('IYS Gateway outbound cron synchronization failed.', [
+                    'store_id' => $storeId,
+                    'exception' => $exception,
+                ]);
             }
         }
     }
